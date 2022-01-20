b7acda3b148c0bb3d371cb409cb43a08d2d3a31d
user86 <user86marc@gmail.com>
2022-01-20 08:31:20 +0200

    complete loggin app

--- lib/src/bloc/bloc.dart
+++ lib/src/bloc/bloc.dart
@@ -12,8 +12,8 @@ import 'package:rxdart/rxdart.dart';
 class Bloc extends Object with Validators {
 // two stream controllers, password and pasword
 // marc the field as privat so that it is hidden from other files in the project
-  final _email = StreamController<String>.broadcast();
-  final _password = StreamController<String>.broadcast();
+  final _email = BehaviorSubject<String>();
+  final _password = BehaviorSubject<String>();
   // onInput -> map. this converts the html event input into a string. so define the input value as type <String>
 
   // define a getter, which is a shortcut for deep drilling into
@@ -29,6 +29,14 @@ class Bloc extends Object with Validators {
   // add/change data, from the sink, to the stream
   Function(String) get changeEmail => _email.sink.add;
   Function(String) get chanegePassword => _password.sink.add;
+
+  submit() {
+    final validEmail = _email.value;
+    final validPassword = _password.value;
+
+    print('Email is $validEmail');
+    print('Password is $validatePassword');
+  }
 
   // close the sinks when done
   // dispose() {
